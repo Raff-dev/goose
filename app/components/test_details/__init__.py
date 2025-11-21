8723ee5319c7c66b794ab8c9503d1e09e467cd6d
Rafał Łazicki <rafal.lazicki99@gmail.com>
2025-11-21 20:01:52 +0100

    remove: Delete deprecated streamlit interface

--- app/components/test_details/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Public interface for test detail components."""
-
-from __future__ import annotations
-
-from app.components.test_details.detail_view import render_test_detail_view
-from app.components.test_details.grid import render_test_details
-
-__all__ = ["render_test_details", "render_test_detail_view"]
